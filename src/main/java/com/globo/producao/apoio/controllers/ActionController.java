096b5a60f860f6ba003ce6f1df9a58f62a6157b8
ludiniz <lucas.diniz@g.globo>
2021-12-05 08:00:30 -0300

    controller da action feito

---
+++ src/main/java/com/globo/producao/apoio/controllers/ActionController.java
@@ -0,0 +1,89 @@
+package com.globo.producao.apoio.controllers;
+
+
+import com.globo.producao.apoio.dtos.requests.ActionRequestDTO;
+import com.globo.producao.apoio.dtos.requests.ProgramRequestDto;
+import com.globo.producao.apoio.dtos.response.ActionResponseDTO;
+import com.globo.producao.apoio.dtos.response.ProgramResponseDto;
+import com.globo.producao.apoio.mappers.ActionMapper;
+import com.globo.producao.apoio.models.Program;
+import com.globo.producao.apoio.services.interfaces.ActionService;
+import com.globo.producao.apoio.utils.exceptions.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+import static org.springframework.http.ResponseEntity.status;
+
+@RestController
+@RequestMapping("/action")
+@RequiredArgsConstructor
+public class ActionController {
+
+    private final ActionService service;
+
+    private final ActionMapper mapper;
+
+    @GetMapping
+    public ResponseEntity<List<ActionResponseDTO>> getActions() {
+
+        return status(HttpStatus.OK).body(mapper.actionListToActionResponseDTOList(service.findAll()));
+
+    }
+
+    @GetMapping(value = "/list/id")
+    public ResponseEntity<ActionResponseDTO> getActionById
+            (@RequestParam final Long id) {
+
+        return status(HttpStatus.OK).body(mapper.actionToActionResponseDTO(service.findById(id)));
+
+    }
+
+    @PostMapping(value = "/save", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity<ActionResponseDTO> saveAction
+            (@Valid @RequestBody final ActionRequestDTO actionRequestDTO){
+
+        return status(HttpStatus.CREATED).body(mapper.actionToActionResponseDTO(
+                service.save(mapper.actionRequestDtoToAction(actionRequestDTO))));
+    }
+
+    @PutMapping(value = "/edit-action/{id}")
+    public ResponseEntity<ActionResponseDTO> updateActionById (
+            @PathVariable(value = "id") final Long id,
+            @Valid @RequestBody final ActionRequestDTO actionRequestDTO) {
+
+        return status(HttpStatus.OK).body(mapper.actionToActionResponseDTO
+                (service.update(id, mapper.actionRequestDtoToAction(actionRequestDTO))));
+
+    }
+
+    @DeleteMapping(value = "/delete-action/{id}")
+    public ResponseEntity<Program> actionProgram (
+            @PathVariable(value = "id") final Long actionId) {
+
+        service.delete(actionId);
+
+        return ResponseEntity.ok().build();
+
+    }
+
+    @GetMapping(value = "/page/{pageNumber}/{pageSize}")
+    public Page<ActionResponseDTO> pageActions(@PathVariable Integer pageNumber, @PathVariable Integer pageSize){
+
+        return mapper.actionToActionResponseDTOPage(service.pageAction(pageNumber,pageSize));
+    }
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/globo/producao/apoio/mappers/ActionMapper.java
@@ -0,0 +1,27 @@
+package com.globo.producao.apoio.mappers;
+
+import com.globo.producao.apoio.dtos.requests.ActionRequestDTO;
+import com.globo.producao.apoio.dtos.requests.ProgramRequestDto;
+import com.globo.producao.apoio.dtos.response.ActionResponseDTO;
+import com.globo.producao.apoio.dtos.response.ProgramResponseDto;
+import com.globo.producao.apoio.models.Action;
+import com.globo.producao.apoio.models.Program;
+import org.mapstruct.Mapper;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ActionMapper {
+
+    Action actionRequestDtoToAction(ActionRequestDTO actionRequestDTO);
+
+    ActionResponseDTO actionToActionResponseDTO(Action action);
+
+    List<ActionResponseDTO> actionListToActionResponseDTOList(List<Action> actions);
+
+    default Page<ActionResponseDTO> actionToActionResponseDTOPage(Page<Action> actions){
+        return actions.map(this::actionToActionResponseDTO);
+    }
+
+}
