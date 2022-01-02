0b5e7cd71f1716ce41cd8b74a377d9652dc9066f
ludiniz <lucas.diniz@g.globo>
2022-01-02 06:19:26 -0300

    controler produto

--- src/main/java/com/globo/producao/apoio/controllers/ProductController.java
+++ src/main/java/com/globo/producao/apoio/controllers/ProductController.java
@@ -6,12 +6,15 @@ import com.globo.producao.apoio.mappers.ProductMapper;
 import com.globo.producao.apoio.models.Product;
 import com.globo.producao.apoio.services.interfaces.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+
+import static org.springframework.http.ResponseEntity.status;
 
 @RestController
 @RequestMapping("/product")
@@ -38,5 +41,23 @@ public class ProductController {
     public ResponseEntity<ProductResponseDTO> saveProgram
             (@Valid @RequestBody final ProductRequestDTO productRequestDTO){
 
+        return status(HttpStatus.CREATED).body(mapper.productToProductResponseDTO(
+                service.save(mapper.productRequestDTOToProduct(productRequestDTO))));
+
+    }
+
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<ProductResponseDTO> updateProduct (@PathVariable("id") final Long id,
+                                                             @Valid @RequestBody final ProductRequestDTO productRequestDTO){
+        return ResponseEntity.ok().body(mapper.productToProductResponseDTO(service
+                .update(id, mapper.productRequestDTOToProduct(productRequestDTO))));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete (@PathVariable("id") final Long id){
+
+        service.delete(id);
+
+        return ResponseEntity.ok().build();
     }
 }
