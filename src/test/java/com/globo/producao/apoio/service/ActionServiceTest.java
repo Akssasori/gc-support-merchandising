69a898da263c46b71760d46216e37becda58d499
Akssasori <diniz3003@gmail.com>
2022-01-11 21:37:48 -0300

    test update

--- src/test/java/com/globo/producao/apoio/service/ActionServiceTest.java
+++ src/test/java/com/globo/producao/apoio/service/ActionServiceTest.java
@@ -6,19 +6,15 @@ import com.globo.producao.apoio.fakerModal.ProgramFaker;
 import com.globo.producao.apoio.models.Action;
 import com.globo.producao.apoio.repositories.*;
 import com.globo.producao.apoio.services.Impls.*;
-import com.globo.producao.apoio.services.interfaces.AgencyService;
-import com.globo.producao.apoio.services.interfaces.ClientService;
-import com.globo.producao.apoio.services.interfaces.ProductService;
-import com.globo.producao.apoio.services.interfaces.ProgramService;
-import com.globo.producao.apoio.utils.exceptions.InsertDataException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -75,6 +71,40 @@ public class ActionServiceTest extends BaseTest {
 
         doReturn(ActionFaker.getAction()).when(actionRepository).save(ActionFaker.getAction());
         Action response = actionService.save(ActionFaker.getAction());
+
+        assertNotNull(response);
+
+    }
+
+    @Test
+    @DisplayName("Should return List of action with success.")
+    public void shouldReturnListAction() throws Exception {
+
+        doReturn(List.of(ActionFaker.getAction())).when(actionRepository).findAll();
+        List<Action> response = actionService.findAll();
+
+        assertNotNull(response);
+
+    }
+
+    @Test
+    @DisplayName("Should return List of action with success.")
+    public void shouldReturnActionFindById() throws Exception {
+
+        doReturn(Optional.of(ActionFaker.getAction())).when(actionRepository).findById(1L);
+        Action response = actionService.findById(1L);
+
+        assertNotNull(response);
+
+    }
+
+    @Test
+    @DisplayName("Should return a update action with success.")
+    public void shouldReturnActionForUpdate() throws Exception {
+
+        doReturn(Optional.of(ActionFaker.getAction())).when(actionRepository).findById(1L);
+        doReturn(ActionFaker.getAction()).when(actionRepository).save(ActionFaker.getAction());
+        Action response = actionService.update(1L,ActionFaker.getAction());
 
         assertNotNull(response);
 
