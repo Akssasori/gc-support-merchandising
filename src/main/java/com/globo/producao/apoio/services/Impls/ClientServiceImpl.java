2d340daa4040de80f0dd24b2fc702a2f5a8a37c1
Akssasori <diniz3003@gmail.com>
2022-02-27 20:01:45 -0300

    ajuste client service

--- src/main/java/com/globo/producao/apoio/services/Impls/ClientServiceImpl.java
+++ src/main/java/com/globo/producao/apoio/services/Impls/ClientServiceImpl.java
@@ -1,24 +1,22 @@
 package com.globo.producao.apoio.services.Impls;
 
 import com.globo.producao.apoio.models.Client;
-import com.globo.producao.apoio.models.Product;
-import com.globo.producao.apoio.models.Program;
 import com.globo.producao.apoio.repositories.ClientRepository;
 import com.globo.producao.apoio.services.interfaces.ClientService;
 import com.globo.producao.apoio.utils.exceptions.*;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.springframework.http.ResponseEntity;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ClientServiceImpl implements ClientService {
 
+    public static final long DEFAULT_REPOSITORY = 1L;
     private final ClientRepository clientRepository;
 
     @Override
@@ -35,10 +33,9 @@ public class ClientServiceImpl implements ClientService {
             }
 
         } else {
+            if (StringUtils.isBlank(client.getName())) {
+                return clientRepository.findById(DEFAULT_REPOSITORY).get();
 
-            if (Objects.isNull(client.getName()) || client.getName().isEmpty()) {
-                Client clientDefault = clientRepository.findById(1L).get();
-                return clientDefault;
             } else {
                 return clientRepository.save(client);
             }
@@ -50,91 +47,45 @@ public class ClientServiceImpl implements ClientService {
 
 
     @Override
-    public List<Client> findAll() throws FindDataException {
-        try {
-            return clientRepository.findAll();
-        } catch (Exception e) {
-            throw new FindDataException(e.getMessage());
-        }
+    public List<Client> findAll() {
+        return clientRepository.findAll();
     }
 
     @Override
     public Client findById(Long id) throws FindDataException {
-        try {
-            return clientRepository.findById(id).orElseThrow(() -> new NoEntityException(id.toString()));
-        } catch (Exception e) {
-            throw new FindDataException(e.getMessage());
-        }
+        return clientRepository.findById(id).orElseThrow(() -> new NoEntityException(id.toString()));
     }
 
     @Override
     public Client update(Long id, Client client) throws UpdateDataException {
 
-        Optional<Client> clientDB;
+        var clientDB = clientRepository.findById(id).orElseThrow(() -> new NoEntityException(id.toString()));
 
-        try {
-            clientDB = clientRepository.findById(id);
-
-            if (clientDB.isPresent()) {
-
-                if (Objects.isNull(client.getName())) {
-
-                    Client clientDefault = clientRepository.findById(1L).get();
-                    return clientDefault;
-                }
-
-                if (Objects.equals(client.getName().trim().toUpperCase(),
-                        clientDB.get().getName().trim().toUpperCase())) {
-                    return clientDB.get();
-                }
-
-                if (client.getName().trim().isEmpty()) {
-                    return clientDB.get();
-
-                } else {
-                    clientDB.get().setId(id);
-                    clientDB.get().setName(client.getName());
-                    clientDB.get().setIdSiscom(client.getIdSiscom());
-                    return clientRepository.save(clientDB.get());
-                }
-            } else {
-                return clientDB.get();
-            }
-
-        } catch (Exception e) {
-            throw new UpdateDataException(e.getMessage());
+        if (Objects.equals(client.getName().trim().toUpperCase(),
+                clientDB.getName().trim().toUpperCase())) {
+            return clientDB;
+        } else {
+            clientDB.setId(id);
+            clientDB.setName(client.getName());
+            clientDB.setIdSiscom(client.getIdSiscom());
+            return clientRepository.save(clientDB);
         }
-
-//        Client clientDB = clientRepository.findById(id).orElseThrow(() -> new NoEntityException(id.toString()));
-//
-//        if (Objects.equals(clientDB.getName().trim().toUpperCase(), client.getName().trim().toUpperCase()) &&
-//                Objects.equals(clientDB.getIdSiscom(), client.getIdSiscom())) {
-//            return clientDB;
-//        } else {
-//            clientDB.setName(client.getName());
-//            clientDB.setId(id);
-//            clientDB.setIdSiscom(client.getIdSiscom());
-//            return clientRepository.save(clientDB);
-//        }
 
     }
 
     @Override
     public void delete(Long id) throws DeleteDataException {
-        try {
-            if (clientRepository.existsById(id)) {
-                clientRepository.deleteById(id);
-            } else {
-                ResponseEntity.notFound();
-            }
-        } catch (Exception e) {
-            throw new DeleteDataException(e.getMessage());
+
+        if (clientRepository.existsById(id)) {
+            clientRepository.deleteById(id);
+        } else {
+            throw new NoEntityException(id.toString());
         }
 
     }
 
     @Override
     public Client findByIdSiscom(Long idSiscom) {
-        return clientRepository.findByIdSiscom(idSiscom).get();
+        return clientRepository.findByIdSiscom(idSiscom).orElseThrow(() -> new NoEntityException(idSiscom.toString()));
     }
 }
